18426670627f8278cf0f76fbde292cc7ff5cdbfa
Patrick <patrick.zhang0502@gmail.com>
2024-08-27 22:09:45 -0400

    Added React App

---
+++ main/src/components/navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Navbar = (props) => {
+    return (<nav class="bg-black">
+    <div className="grid grid-cols-2 h-16 gap-4 px-6">
+                <div className="flex justify-self-start px-8 pt-5">
+                    <h1 className="flex-none text-white">{props.name}</h1>
+                </div>
+            </div>
+            </nav>);
+};
+
+export default Navbar;
